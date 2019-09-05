b230c47cafea099e14ca247a3fee4521631e785d
chadthom7 <chadthom7@gmail.com>
2019-09-05 14:21:26 -0400

    Server v1

--- Server.java
+++ Server.java
@@ -8,7 +8,9 @@ public class Server
 /* initialize socket and input stream */
 private Socket socket = null; 
 private ServerSocket server = null;
+private BufferedReader input = null;
 private DataInputStream in = null; 
+private DataOutputStream out = null; 
 
 /* constructor with port */
 public Server(int port) 
@@ -25,32 +27,44 @@ public Server(int port)
 	System.out.println("Waiting for a client ..."); 
 	try {
 		socket = server.accept(); 
-		System.out.println("Client accepted"); 
+		System.out.println("Client accepted"); //Connected 
 		
+		// terminal input
+		input = new BufferedReader(new InputStreamReader(System.in));
+		
+		// sends output
+		out = new DataOutputStream(socket.getOutputStream());
+
 		/* takes input from the client socket */
 		in = new DataInputStream( 
 		    new BufferedInputStream(socket.getInputStream())); 
 		
-		String line = ""; 
-		
+		String i_line = ""; 
+		String o_line = "";	
 		/* reads message from client until "Over" is sent */
-		while (!line.equals("Over")) 
+		while (!i_line.equals("Over") && !o_line.equals("Over")) 
 		{ 
-		        line = in.readUTF(); 
-		        System.out.println(line); 
+		        i_line = in.readUTF(); 
+		        System.out.println(i_line); 
+			//Data to Client
+			o_line = input.readLine();
+			out.writeUTF(o_line);
+
 		} 
 		System.out.println("Closing connection"); 
 		
 		/* close connection */
+		input.close();
 		socket.close(); 
 		in.close(); 
-
+		out.close();
 	} catch(EOFException i) { 
 	    System.out.println(i); 
 	} 
 	catch(Exception i) { 
 	    System.out.println(i); 
-	} 
+	}
+
 }
 
 public static void main(String args[]) 
